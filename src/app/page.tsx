49f574581af9d2e4e6c968b66899cc9265dacd93
Étienne Doré <119957024+etiennedo@users.noreply.github.com>
2025-02-01 00:07:28 -0500

    added nojekyll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/blog/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -33,7 +33,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/blog/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -59,7 +59,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/blog/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -74,7 +74,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/blog/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -89,7 +89,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="/blog/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
